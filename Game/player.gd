eb497b111ea895132480f000a3baaf92a66a41cb
swingbass <chs092704@gmail.com>
2024-08-27 20:37:59 -0500

    Character Movement Overhaul
    Solved Github Issue "Improve Player Character". Changed handling of momentum/acceleration, added a quick dash option, added wall slide and wall jump, and coyote time implementation. Also created adequate testing terrain for player character.

--- Game/player.gd
+++ Game/player.gd
@@ -2,11 +2,14 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+var acceleration = 25
 
 var dash_speed = 700
 var is_dashing = false
 
 var can_jump = false
+
+var wall_jump_power = 500
 
 func _on_coyote_timer_timeout():
 	can_jump = false
@@ -35,20 +38,40 @@ func _physics_process(delta):
 	
 	if $dash_timer.time_left == 0:
 		_on_dash_timer_timeout()
-	if Input.is_action_just_pressed("Dash") and is_dashing == false and $dash_timer.is_stopped():
+		
+	if $dash_cooldown_timer.time_left == 0:
+		_on_dash_cooldown_timer_timeout()
+		
+	if Input.is_action_just_pressed("Dash") and is_dashing == false and $dash_timer.is_stopped() and $dash_cooldown_timer.time_left == 0:
 			$dash_timer.start()
+			$dash_cooldown_timer.start()
 			is_dashing = true
 	if direction:
 		if is_dashing == true:
+			velocity.y = 0;
 			velocity.x = direction * dash_speed
 			
 		else:
-			velocity.x = direction * SPEED
+			velocity.x = move_toward(velocity.x, direction * SPEED, acceleration)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, acceleration)
 
 	move_and_slide()
+	
+	if is_on_wall() and !is_on_floor():
+		velocity.y = 50
+	
+	if is_on_wall() and Input.is_action_pressed("jump"):
+		$wall_timer.start()
+		velocity.y = JUMP_VELOCITY
+		velocity.x = get_wall_normal().x * wall_jump_power
+		can_jump = false
+		
 
 func _on_dash_timer_timeout():
 	is_dashing = false
 	$dash_timer.stop()
+
+
+func _on_dash_cooldown_timer_timeout():
+	$dash_cooldown_timer.stop()
