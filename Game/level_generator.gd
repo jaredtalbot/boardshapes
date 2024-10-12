f4cfe2aa5130a99c8a3899dc40a3e6f4e8a4280c
Jared Talbot <jared.talbot-2@selu.edu>
2024-10-12 01:06:11 -0500

    wip: web file select done

---
+++ Game/level_generator.gd
@@ -0,0 +1,31 @@
+extends Node
+
+func generate_nodes(json_string: String) -> bool:
+	var json = JSON.parse_string(json_string)
+	if json is not Array:
+		return false
+	if !json.all(checkItem):
+		return false
+	var level = Node.new()
+	for item in json:
+		var region = Node2D.new()
+		var byte_pool = Marshalls.base64_to_raw(item["regionImage"])
+		var img = Image.new()
+		img.load_png_from_buffer(byte_pool)
+		var tex_rect = TextureRect.new()
+		tex_rect.texture = ImageTexture.create_from_image(img)
+		region.add_child(tex_rect)
+		var collision = CollisionPolygon2D.new()
+		var mesh = item["mesh"] as Array
+		var vectormesh = mesh.map(func(v: Dictionary): return Vector2(v["x"], v["y"]))
+		collision.polygon = vectormesh
+		var col = StaticBody2D.new()
+		col.add_child(collision)
+		region.add_child(col)
+		region.position = Vector2(item["cornerX"], item["cornerY"])
+		level.add_child(region)
+	return true
+
+func checkItem(item: Variant) -> bool:
+	return item is Dictionary and item.get("regionImage") is String and item.get("mesh") is Array \
+		and item["mesh"].all(func(m): m is Dictionary and m.has_all(["x", "y"])) and item.has_all(["cornerX", "cornerY"])
---
+++ Game/start_menu.gd
@@ -0,0 +1,29 @@
+extends Control
+
+@onready var web_pick_image_file = $WebPickImageFile
+@onready var image_confirmation = $ImageConfirmation
+
+#todo: add non-web file picker
+
+func _on_upload_image_button_pressed():
+	web_pick_image_file.show()
+	
+func _on_back_button_pressed():
+	get_tree().change_scene_to_file("res://main.tscn")
+
+func _on_web_pick_image_file_file_loaded(content: PackedByteArray, filename: String):
+	var image = Image.new()
+	var error: Error
+	if filename.ends_with("png"):
+		error = image.load_png_from_buffer(content)
+	elif filename.ends_with("jpg") or filename.ends_with("jpeg"):
+		error = image.load_jpg_from_buffer(content)
+	
+	if error != 0:
+		return
+	
+	image_confirmation.set_image(image)
+	image_confirmation.show()
+
+func _on_image_confirmation_confirmed():
+	pass #todo
