a0a2707ab01377b6d9d3833b2bbc35805d1d4e0a
Luke White <lukewhi225@gmail.com>
2024-10-29 21:45:05 -0500

    First commit Setting up flags,

--- WebApp/cli_processing/image_processing.go
+++ WebApp/cli_processing/image_processing.go
@@ -2,41 +2,46 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"image"
-	"log"
-	"os"
-	"strings"
 )
 
+var rFlag = flag.Int(r, image.Bounds, "this should be checking image bounds and resizing ")
+var sFlag = flag.String(s, userFilePath, "Should allow a user to input a file path. Unsure on this")
+var mFlag = flag.Bool(m, false, "Should meshify regions.")
+var fileInput = flag.Args()
+
 func main() {
-	var simplified = flag.String("s", "./test_images/out.png", "For simplification to outputfile")
-	var meshes = flag.Bool("m", false, "For mesh conversion")
-	var resize = flag.Bool("r", false, "For Sizing")
-	var file_intake = flag.Arg(0)
+	flag.Parse()
 
-	if len(file_intake) == 0 {
-		log.Println("No File Name provided")
-		os.Exit(1)
-
-	} else if !strings.Contains(file_intake, ".jpeg") && !strings.Contains(file_intake, ".png") {
-		file, fileErr := os.Open(file_intake)
-
-		if (fileErr != nil) {
-			panic(fileErr)
-			
-		}
-		defer file.Close()
-
-		img, format, err := image.Decode(file)
-		if err != nil {
-			fmt.Print("error decoding" + err)
-
-		} else {
-			flag.Parse()
-		}
-	}
 }
+
+//var simplified = flag.String("s", "./test_images/out.png", "For simplification to outputfile")
+//var meshes = flag.Bool("m", false, "For mesh conversion")
+//var resize = flag.Bool("r", false, "For Sizing")
+//var file_intake = flag.Arg(0)
+
+//if len(file_intake) == 0 {
+//	log.Println("No File Name provided")
+//	os.Exit(1)
+
+//} else if !strings.Contains(file_intake, ".jpeg") && !strings.Contains(file_intake, ".png") {
+//	file, fileErr := os.Open(file_intake)
+//
+//		if fileErr != nil {
+//			panic(fileErr)
+
+//		}
+//		defer file.Close()
+
+//		img, format, err := image.Decode(file)
+//		if err != nil {
+//			fmt.Print(err)
+
+//		} else {
+//			flag.Parse()
+//		}
+//	}
+//}
 
 //else if !strings.Contains(file_intake, ".jpeg") && !strings.Contains(file_intake, ".png") {
 //image.Decode(file_intake)
