1099bd0985c3bd77b614935e76d12fdec6c1d506
daydreamxr <87155431+day-dreamxr@users.noreply.github.com>
2024-11-06 17:31:13 -0600

    Update Game/level.gd
    Co-authored-by: Jared <45642475+codeJester27@users.noreply.github.com>

--- Game/level.gd
+++ Game/level.gd
@@ -73,7 +73,7 @@ func go_back():
 	get_tree().change_scene_to_file("res://start_menu.tscn")
 
 func _on_exit_to_main_menu_button_pressed():
-	get_tree().change_scene_to_file("res://menu.tscn")
+	get_tree().change_scene_to_file("res://main.tscn")
 
 func _set_player_start():
 	var player = $Player
