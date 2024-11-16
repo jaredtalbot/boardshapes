2998e718be241a8ead66fb04e09bf033a1c31348
Luke White <lukewhi225@gmail.com>
2024-11-16 15:01:55 -0600

    Almost done needs finishing touches.

--- WebApp/cli_processing/image_processing.go
+++ WebApp/cli_processing/image_processing.go
@@ -2,15 +2,14 @@ package main
 
 import (
 	"codejester27/cmps401fa2024/web-app/processing"
+
 	"flag"
 	"fmt"
-	"mime/quotedprintable"
-	"path/filepath"
-
 	"image"
 	"image/jpeg"
 	"image/png"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
@@ -19,63 +18,83 @@ var sFlag = flag.String("s", "", "Should allow a user to input a file path for o
 var mFlag = flag.Bool("m", false, "Should meshify regions.")
 
 func main() {
+
 	flag.Parse()
 	fileInput := flag.Args()
-	fileToOutput, inputDir := file_opener(fileInput)
+	fileToOutput, err := fileOpenerDecoder(fileInput)
 
-
-
-	if *rFlag {
-		newResizedImage, err := processing.ResizeImage(fileToOutput)
-			if err != nil {
-				"fix your stuff bruh"
-			}
-
-
-		// umm image get image resize image.
-		// should allow me to resize image but should be after file Open.
-		//fileExtension := filepath.Ext(fileToOutput)
-		//if fileExtension == ".png" || ".jpeg" {
-			//fileDecoded, err = image.Decode(fileToOutput)
-			//err, fileResized := processing.ResizeImage(fileDecoded)
-		
-	
-
+	if err != nil {
+		panic(err)
 	}
 
-	image_output(fileToOutput, inputDir)
+	if *rFlag {
+		img, err := processing.ResizeImage(fileToOutput)
+		if err != nil {
+			fmt.Println("fix your stuff bruh")
+		}
+		if *mFlag {
+			fileRegioned, regionCount := processing.SimplifyImage(img)
+			fmt.Println(regionCount)
+			outputFile := fileEncoder(fileRegioned)
+			image_output(outputFile, filepath.Dir(fileInput[0]))
+		} else {
+			outputFile := fileEncoder(img)
+			image_output(outputFile, filepath.Dir(fileInput[0]))
+		}
+	}
 
 	fmt.Println("1:", *rFlag)
 	fmt.Println("2:", *sFlag)
 	fmt.Println("3:", *mFlag)
 	fmt.Println("4:", fileInput)
 
-	
 }
-	
 
-func file_opener_decoder(fileInput []string) (*os.File, string) {
-	
+func fileOpenerDecoder(fileInput []string) (image.Image, error) {
+
 	joinedFileName := strings.Join(fileInput, "")
 	fileTaken, err := os.Open(joinedFileName)
 	if err != nil {
 		panic(err)
 	}
-	//fmt.Println(fileTaken)
-	inputDir := filepath.Dir(joinedFileName)
+	defer fileTaken.Close()
 
-	fileExtension := filepath.Ext(fileTaken)
-	if fileExtension == ".png" || ".jpeg" {
-		img, extName, err :=image.Decode(fileTaken)
+	fileExtension := filepath.Ext(joinedFileName)
+	if fileExtension == ".png" || fileExtension == ".jpeg" {
+		img, _, err := image.Decode(fileTaken)
 		if err != nil {
 			panic(err)
 		}
 
-
-	return img, inputDir
+		return img, nil
+	}
+	return nil, fmt.Errorf("unsuppported file format")
 }
 
+func fileEncoder(img image.Image) *os.File {
 
+	var outputPath string
+	if *sFlag != "" {
+		outputPath = *sFlag
+	} else {
+		outputPath = "output.png"
+	}
+	outputFile, err := os.Create(outputPath)
+	if err != nil {
+		panic(err)
+	}
+	ext := strings.ToLower(filepath.Ext(outputPath))
+	if ext == ".png" {
+		err = png.Encode(outputFile, img)
+	} else if ext == ".jpeg" || ext == ".jpg" {
+		err = jpeg.Encode(outputFile, img, &jpeg.Options{Quality: 90})
+
+	}
+	if err != nil {
+		panic(err)
+	}
+	return outputFile
+}
 
 func image_output(fileToOutput *os.File, inputDir string) {
 
@@ -88,5 +107,3 @@ func image_output(fileToOutput *os.File, inputDir string) {
 	outputFilePath := filepath.Join(outputDir, filepath.Base(fileToOutput.Name()))
 	fmt.Printf("Output file path: %s\n", outputFilePath)
 }
-
-
