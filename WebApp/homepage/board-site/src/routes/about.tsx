6005aa7f262045f0e9a12040107bafa5f4629421
chschlich <cohen.schlicher@selu.edu>
2024-11-20 13:26:45 -0600

    boardsite update
    changes formatting pretty signifcantly

--- WebApp/homepage/board-site/src/routes/about.tsx
+++ WebApp/homepage/board-site/src/routes/about.tsx
@@ -1,33 +1,35 @@
 import { Link } from "react-router-dom";
-import "./about.css";
+import "./root.css";
 
 function About() {
   return (
     <>
-      <h1>About The Devs:</h1>
-      <p className="AboutTitle">Jared</p>
-      <p className="AboutDescription">
-        Jared is a Fifth-Semester Junior majoring in Computer Science. He is the
-        project leader.
-      </p>
-      <p className="AboutTitle">Cohen</p>
-      <p className="AboutDescription">
-        Cohen is a Fifth-Semester Junior majoring in Computer Science. He made
-        this website!
-      </p>
-      <p className="AboutTitle">Luke</p>
-      <p className="AboutDescription">
-        Luke is a Fifth-Semester Junior majoring in Computer Science. He's hard
-        at work on the CLI tool!
-      </p>
-      <p className="AboutTitle">Zach</p>
-      <p className="AboutDescription">
-        Zach is a Fifth-Semester Junior majoring in Computer Science. He made
-        the Boardwalk mobile port!
-      </p>
-      <Link to={`/`} className="link">
-        Return Home!
-      </Link>
+      <div className="body1">
+        <h1>About The Devs:</h1>
+        <p className="AboutTitle">Jared</p>
+        <p className="AboutDescription">
+          Jared is a Fifth-Semester Junior majoring in Computer Science. He is
+          the project leader!
+        </p>
+        <p className="AboutTitle">Cohen</p>
+        <p className="AboutDescription">
+          Cohen is a Fifth-Semester Junior majoring in Computer Science. He made
+          this website!
+        </p>
+        <p className="AboutTitle">Luke</p>
+        <p className="AboutDescription">
+          Luke is a Fifth-Semester Junior majoring in Computer Science. He's
+          hard at work on the CLI tool!
+        </p>
+        <p className="AboutTitle">Zach</p>
+        <p className="AboutDescription">
+          Zach is a Fifth-Semester Junior majoring in Computer Science. He made
+          the Boardwalk mobile port!
+        </p>
+        <Link to={`/`} className="link">
+          Return Home!
+        </Link>
+      </div>
     </>
   );
 }
--- WebApp/homepage/board-site/src/routes/root.tsx
+++ WebApp/homepage/board-site/src/routes/root.tsx
@@ -4,35 +4,81 @@ import githubLogo from "../img/github.svg";
 import boardboxLogo from "../img/BoardBox.svg";
 import "./root.css";
 import { Outlet, Link } from "react-router-dom";
+import Headroom from "react-headroom";
 
 function Root() {
   return (
     <>
-      <img src={boardmeshLogo} className="logoMain" alt="Boardwalk logo" />
-      <h1>
-        Boardmesh: The web API that takes image files and translates them into
-        polygonal meshes!
-      </h1>
-      <div>
-        <a href="/boardwalk/" target="_blank">
-          <img src={boardwalkLogo} className="logo" alt="Boardwalk logo" />
-        </a>
-        <a href="/manual/" target="_blank">
-          <img src={boardmeshLogo} className="logo" alt="Boardmesh logo" />
-        </a>
-        <a href="/boardbox/" target="_blank">
-          <img src={boardboxLogo} className="logo" alt="Boardbox logo" />
-        </a>
-        <a href="https://github.com/codeJester27/cmps401fa2024" target="_blank">
-          <img src={githubLogo} className="logo" alt="Github logo" />
-        </a>
+      <Headroom
+        wrapperStyle={{ marginBottom: 35 }}
+        style={{
+          background: "rgb(220, 220, 220)",
+        }}
+      >
+        <div style={{ padding: 10 }}>
+          <h1
+            style={{
+              margin: 0,
+              color: "rgb(252, 253, 254)",
+            }}
+          >
+            <div>
+              <a href="/boardwalk/" target="_blank">
+                <img
+                  src={boardwalkLogo}
+                  className="logo"
+                  alt="Boardwalk logo"
+                />
+              </a>
+              <a href="/manual/" target="_blank">
+                <img
+                  src={boardmeshLogo}
+                  className="logo"
+                  alt="Boardmesh logo"
+                />
+              </a>
+              <a href="/boardbox/" target="_blank">
+                <img src={boardboxLogo} className="logo" alt="Boardbox logo" />
+              </a>
+              <a
+                href="https://github.com/codeJester27/cmps401fa2024"
+                target="_blank"
+              >
+                <img src={githubLogo} className="logo" alt="Github logo" />
+              </a>
+            </div>
+          </h1>
+        </div>
+      </Headroom>
+      <div className="body1">
+        <img src={boardmeshLogo} className="logoMain" alt="Boardwalk logo" />
+        <h1>
+          Boardmesh is a free and open-source API that can be used to create
+          suitable, color-corrected, collidable shapes for physics simulators
+          and simplify an image into a restricted color palette consisting of
+          black, white, red, green, and blue.
+        </h1>
+        <h1>
+          Tapping "Boardwalk" will take you to our game implementation of the
+          Boardmesh API, Boardwalk. Boardwalk is a single-screen platformer that
+          allows the player to traverse their newly created meshes.
+        </h1>
+        <h1>
+          Tapping "Boardmesh" will take you to the User Manual for Boardwalk.
+        </h1>
+        <h1>
+          Tapping the third logo will take you to our Physics Sim, Boardbox.
+          This allows you to interact with your new meshes in a very direct
+          manner.
+        </h1>
+        <h1>Tapping the Github logo will take you to our GitHub repository.</h1>
+        <p className="read-the-docs">
+          If you wanna know more about the developers, click the link below!
+        </p>
+        <Link to={`/about`} className="link">
+          Click Here!
+        </Link>
       </div>
-      <p className="read-the-docs">
-        If you wanna know more about the developers, click the link below!
-      </p>
-      <Link to={`/about`} className="link">
-        Click Here!
-      </Link>
       <div id="detail">
         <Outlet />
       </div>
