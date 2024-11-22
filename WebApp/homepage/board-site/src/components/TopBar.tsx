c6e63bc0ea59d84242b2f32f25953d1b4f365270
Jared Talbot <jared.talbot-2@selu.edu>
2024-11-21 19:48:54 -0600

    fix: some stuff

---
+++ WebApp/homepage/board-site/src/components/TopBar.tsx
@@ -0,0 +1,32 @@
+import boardwalkLogo from "../img/boardwalk.svg";
+import boardmeshLogo from "../img/boardmesh.svg";
+import githubLogo from "../img/github.svg";
+import boardboxLogo from "../img/BoardBox.svg";
+import Headroom from "react-headroom";
+
+export default function TopBar() {
+  return (
+    <Headroom className="topbar">
+      <nav>
+        <div className="link-container">
+          <a href="/boardwalk/" target="_blank">
+            <img src={boardwalkLogo} className="logo" alt="Boardwalk logo" />
+          </a>
+          <a href="/manual/" target="_blank">
+            <img src={boardmeshLogo} className="logo" alt="Boardmesh logo" />
+          </a>
+          <a href="/boardbox/" target="_blank">
+            <img src={boardboxLogo} className="logo" alt="Boardbox logo" />
+          </a>
+          <a
+            href="https://github.com/codeJester27/cmps401fa2024"
+            target="_blank"
+            rel="noopener"
+          >
+            <img src={githubLogo} className="logo" alt="Github logo" />
+          </a>
+        </div>
+      </nav>
+    </Headroom>
+  );
+}
