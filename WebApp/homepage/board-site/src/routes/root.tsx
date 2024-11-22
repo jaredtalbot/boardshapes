b4dc6db5bec03729bbbaec0331b9f769031a0277
Jared Talbot <jared.talbot-2@selu.edu>
2024-11-21 20:05:52 -0600

    feat: fix home page layout

--- WebApp/homepage/board-site/src/routes/root.tsx
+++ WebApp/homepage/board-site/src/routes/root.tsx
@@ -7,26 +7,28 @@ function Root() {
     <>
       <div className="body1">
         <img src={boardmeshLogo} className="logoMain" alt="Boardwalk logo" />
-        <p>
-          Boardmesh is a free and open-source API that can be used to create
-          suitable, color-corrected, collidable shapes for physics simulators
-          and simplify an image into a restricted color palette consisting of
-          black, white, red, green, and blue.
-        </p>
-        <p>
-          Tapping "Boardwalk" will take you to our game implementation of the
-          Boardmesh API, Boardwalk. Boardwalk is a single-screen platformer that
-          allows the player to traverse their newly created meshes.
-        </p>
-        <p>
-          Tapping "Boardmesh" will take you to the User Manual for Boardwalk.
-        </p>
-        <p>
-          Tapping the third logo will take you to our Physics Sim, Boardbox.
-          This allows you to interact with your new meshes in a very direct
-          manner.
-        </p>
-        <p>Tapping the Github logo will take you to our GitHub repository.</p>
+        <div className="boardmesh-text-container">
+          <p>
+            Boardmesh is a free and open-source API that can be used to create
+            suitable, color-corrected, collidable shapes for physics simulators
+            and simplify an image into a restricted color palette consisting of
+            black, white, red, green, and blue.
+          </p>
+          <p>
+            Tapping "Boardwalk" will take you to our game implementation of the
+            Boardmesh API, Boardwalk. Boardwalk is a single-screen platformer
+            that allows the player to traverse their newly created meshes.
+          </p>
+          <p>
+            Tapping "Boardmesh" will take you to the User Manual for Boardwalk.
+          </p>
+          <p>
+            Tapping the third logo will take you to our Physics Sim, Boardbox.
+            This allows you to interact with your new meshes in a very direct
+            manner.
+          </p>
+          <p>Tapping the Github logo will take you to our GitHub repository.</p>
+        </div>
         <p className="read-the-docs">
           If you wanna know more about the developers, click the link below!
         </p>
