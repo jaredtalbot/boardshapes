93d6c6f306a4795ebbe9fc1a0f28a5736e52ceec
day-dreamxr <zachmeister2143@gmail.com>
2024-11-24 13:16:26 -0600

    colorblind mode ver. 1

--- Game/menu.gd
+++ Game/menu.gd
@@ -5,6 +5,8 @@ func _ready():
 		$MarginContainer/VBoxContainer/Exit.hide()
 	if RenderingServer.get_default_clear_color() == Color(0, 0, 0, 1):
 		$OptionsWindow/DarkMode.set_pressed_no_signal(true)
+	if ProjectSettings.get_setting("rendering/environment/defaults/color_blind_mode") == true:
+		$OptionsWindow/ColorblindMode.set_pressed_no_signal(true)
 	
 func _on_start_pressed():
 	get_tree().change_scene_to_file("res://start_menu.tscn")
@@ -24,3 +26,9 @@ func _on_dark_mode_toggled(toggled):
 		RenderingServer.set_default_clear_color(Color(0, 0, 0, 1))
 	else:
 		RenderingServer.set_default_clear_color(Color(1, 1, 1, 1))
+
+func _on_colorblind_mode_toggled(toggled):
+	if toggled:
+		ProjectSettings.set_setting("rendering/environment/defaults/color_blind_mode", true)
+	else:
+		ProjectSettings.set_setting("rendering/environment/defaults/color_blind_mode", false)
