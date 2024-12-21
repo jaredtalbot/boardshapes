31242c61b991f031ae8e3097e9d5758d03ef4e4b
Jared Talbot <jared.talbot-2@selu.edu>
2024-12-21 02:26:11 -0600

    wip: galunga

--- Game/unlocks.gd
+++ Game/unlocks.gd
@@ -1,6 +1,30 @@
 extends Node
 
+var unlocked_hat_paths := PackedStringArray()
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass
+func _ready():
+	load_unlocks()
+
+func load_unlocks():
+	var file := FileAccess.open("user://unlocks.json", FileAccess.READ)
+	var err := FileAccess.get_open_error()
+	if err != OK:
+		if err != ERR_FILE_NOT_FOUND:
+			pass #add some kind of alert to the user that unlocks can't load
+		save_unlocks()
+		return 
+	
+	var json = JSON.parse_string(file.get_as_text())
+	if json is Dictionary and json.get("hats") is Array:
+		unlocked_hat_paths = PackedStringArray(json.hats)
+	else:
+		save_unlocks()
+
+func save_unlocks():
+	var file := FileAccess.open("user://unlocks.json", FileAccess.WRITE)
+	var err := FileAccess.get_open_error()
+	if err:
+		return #add some kind of alert to the user that unlocks can't save
+	file.store_string(JSON.stringify({
+		"hats": unlocked_hat_paths
+	}))
