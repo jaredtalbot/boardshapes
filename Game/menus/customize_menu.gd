ef6d3a1d7696014c35a1c9f8b62d2fe2b1cae9d6
Jared Talbot <jared.talbot-2@selu.edu>
2025-01-23 14:36:17 -0600

    feat: hat info display

--- Game/menus/customize_menu.gd
+++ Game/menus/customize_menu.gd
@@ -7,6 +7,7 @@ const HAT_PREVIEW = preload("res://hats/hat_preview.tscn")
 @onready var hat_select = %HatSelect
 
 func _ready():
+	$HatInfoDisplay.hide()
 	cosmetic_preview.equip_hat(Preferences.hat_scene)
 	Preferences.hat_scene_changed.connect(cosmetic_preview.equip_hat)
 	
@@ -14,10 +15,24 @@ func _ready():
 		var hat_preview = HAT_PREVIEW.instantiate()
 		hat_preview.load_hat_from_json(hat_json)
 		hat_select.add_child(hat_preview)
-		hat_preview.pressed.connect(set_hat.bind(hat_preview.hat_scene))
+		hat_preview.pressed.connect(set_hat.bind(hat_preview.hat_scene, hat_preview.unlocked))
+		hat_preview.focus_entered.connect(set_hat_info_display.bind(hat_preview.hat_name, hat_preview.hat_description, hat_preview.hat_unlock_hint))
 
-func set_hat(new_hat: PackedScene):
-	Preferences.hat_scene = new_hat
+func set_hat(new_hat: PackedScene, unlocked: bool):
+	if unlocked:
+		Preferences.hat_scene = new_hat
+
+func set_hat_info_display(hat_name: String, hat_description: String, hat_unlock_hint: String):
+	$HatInfoDisplay/HatNameLabel.text = hat_name
+	$HatInfoDisplay/HatDescriptionLabel.text = hat_description
+	$HatInfoDisplay/HatUnlockHintLabel.text = hat_unlock_hint
+	if not hat_unlock_hint or hat_unlock_hint == "???":
+		$HatInfoDisplay/UnlockHintSeparator.hide()
+		$HatInfoDisplay/HatUnlockHintLabel.hide()
+	else:
+		$HatInfoDisplay/UnlockHintSeparator.show()
+		$HatInfoDisplay/HatUnlockHintLabel.show()
+	$HatInfoDisplay.show()
 
 func _on_back_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://menus/start_menu.tscn")
