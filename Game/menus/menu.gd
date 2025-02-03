b03f2c59dce948c733bbd12e4e423daeee1c1c2e
Zachary Gros <zachmeister2143@gmail.com>
2025-02-03 10:18:13 -0600

    lag fixed i think
    it doesn't seem to anymore

--- Game/menus/menu.gd
+++ Game/menus/menu.gd
@@ -1,6 +1,13 @@
 extends Control
 
+@onready var hat_dir = DirAccess.open("res://hats")
+
 func _ready():
+	for path in hat_dir.get_directories():
+		var hat_path = str("res://hats/"+path+"/"+path+".tscn")
+		print(hat_path)
+		ResourceLoader.load(hat_path)
+		
 	Music.stop_all_layers()
 	if OS.has_feature("web"):
 		$MarginContainer/VBoxContainer/Exit.hide()
