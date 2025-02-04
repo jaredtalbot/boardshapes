54940f0ef589cbc25f2e583786a34e5e6d67d4c5
chschlich <cohen.schlicher@selu.edu>
2025-02-03 18:38:28 -0600

    XBOX LIVE
    XBOX LIVE

--- Game/hats/goggles/goggles-unlock-checker.gd
+++ Game/hats/goggles/goggles-unlock-checker.gd
@@ -8,11 +8,11 @@ static func get_hat_id() -> String:
 func _jumped_or_dashed():
 	has_jumped_or_dashed = true
 
-func _c_level_complete():
-	if has_jumped_or_dashed == false:
+func _c_level_complete(level: Level):
+	if has_jumped_or_dashed == false and level.current_campaign_level == "res://campaign/c'slvl.boardwalk":
 		Unlocks.unlock_hat("goggles")
 
 func _connect_level_signals(level: Level):
 	level.loaded.connect(func(): level.player.jumped.connect(_jumped_or_dashed); level.player.dashed.connect(_jumped_or_dashed))
 	has_jumped_or_dashed = false
-	level.completed.connect(_c_level_complete)
+	level.completed.connect(_c_level_complete.bind(level))
