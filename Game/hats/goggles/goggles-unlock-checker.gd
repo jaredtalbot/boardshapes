275164d7634ec717ffcb32575270884143f97310
chschlich <cohen.schlicher@selu.edu>
2025-02-06 20:31:32 -0600

    goggles fix
    snuck in a hitbox fit too

--- Game/hats/goggles/goggles-unlock-checker.gd
+++ Game/hats/goggles/goggles-unlock-checker.gd
@@ -7,6 +7,9 @@ static func get_hat_id() -> String:
 
 func _jumped_or_dashed():
 	has_jumped_or_dashed = true
+	
+func _died():
+	has_jumped_or_dashed = false
 
 func _c_level_complete(level: Level):
 	if has_jumped_or_dashed == false and level.current_campaign_level == "res://campaign/c'slvl.boardwalk":
@@ -14,5 +17,6 @@ func _c_level_complete(level: Level):
 
 func _connect_level_signals(level: Level):
 	level.loaded.connect(func(): level.player.jumped.connect(_jumped_or_dashed); level.player.dashed.connect(_jumped_or_dashed))
+	level.loaded.connect(func(): level.player.died.connect(_died))
 	has_jumped_or_dashed = false
 	level.completed.connect(_c_level_complete.bind(level))
