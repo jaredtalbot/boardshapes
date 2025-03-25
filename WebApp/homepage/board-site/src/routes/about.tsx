af75650d21c66d27116238b806a13a1e1e865595
chschlich <cohen.schlicher@selu.edu>
2025-03-25 18:33:46 -0500

    grunt work

--- WebApp/homepage/board-site/src/routes/about.tsx
+++ WebApp/homepage/board-site/src/routes/about.tsx
@@ -1,6 +1,11 @@
 import { Link } from "react-router-dom";
 import "./root.css";
 import { useMemo } from "react";
+import pirateHat from "../img/pirate.png";
+import boardgoggle from "../img/boardgoggle.png";
+import jester from "../img/jester.png";
+import wegahat from "../img/wegahat.png";
+import colander from "../img/colander.png";
 
 function About() {
   const todayHash = useMemo(() => {
@@ -19,28 +24,90 @@ function About() {
     <>
       <div className="body1">
         <h1>About The Devs:</h1>
-        <a className="AboutTitle" href={`/boardwalk?unlock=${todayHash}`}>
-          Jared
-        </a>
-        <p className="AboutDescription">
-          Jared is a Fifth-Semester Junior majoring in Computer Science. He is
-          the project leader!
-        </p>
-        <p className="AboutTitle">Cohen</p>
-        <p className="AboutDescription">
-          Cohen is a Fifth-Semester Junior majoring in Computer Science. He made
-          this website!
-        </p>
-        <p className="AboutTitle">Luke</p>
-        <p className="AboutDescription">
-          Luke is a Fifth-Semester Junior majoring in Computer Science. He's
-          hard at work on the CLI tool!
-        </p>
-        <p className="AboutTitle">Zach</p>
-        <p className="AboutDescription">
-          Zach is a Fifth-Semester Junior majoring in Computer Science. He made
-          the Boardwalk mobile port!
-        </p>
+        <div className="AboutGroup">
+          <a className="hatlink" href={`/boardwalk?unlock=${todayHash}`}>
+            <img className="hat" src={jester}></img>
+          </a>
+          <div className="AboutTitleAndDescription">
+            <p className="AboutTitle">Jared</p>
+            <p className="AboutDescription">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Non
+              veritatis dolorum assumenda officia nobis modi eos, neque enim
+              sapiente laborum voluptates asperiores quis reiciendis maxime
+              ducimus mollitia ullam? Placeat, tenetur. Lorem ipsum dolor sit,
+              amet consectetur adipisicing elit. Assumenda est accusamus
+              temporibus molestiae in nobis illo officia officiis obcaecati
+              tenetur sequi itaque mollitia ut placeat facere, similique
+              laudantium doloribus velit.
+            </p>
+          </div>
+        </div>
+        <div className="AboutGroup">
+          <img className="hat" src={boardgoggle}></img>
+          <div className="AboutTitleAndDescription">
+            <p className="AboutTitle">Cohen</p>
+            <p className="AboutDescription">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Non
+              veritatis dolorum assumenda officia nobis modi eos, neque enim
+              sapiente laborum voluptates asperiores quis reiciendis maxime
+              ducimus mollitia ullam? Placeat, tenetur. Lorem ipsum dolor sit,
+              amet consectetur adipisicing elit. Assumenda est accusamus
+              temporibus molestiae in nobis illo officia officiis obcaecati
+              tenetur sequi itaque mollitia ut placeat facere, similique
+              laudantium doloribus velit.
+            </p>
+          </div>
+        </div>
+        <div className="AboutGroup">
+          <img className="hat" src={wegahat}></img>
+          <div className="AboutTitleAndDescription">
+            <p className="AboutTitle">Luke</p>
+            <p className="AboutDescription">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Non
+              veritatis dolorum assumenda officia nobis modi eos, neque enim
+              sapiente laborum voluptates asperiores quis reiciendis maxime
+              ducimus mollitia ullam? Placeat, tenetur. Lorem ipsum dolor sit,
+              amet consectetur adipisicing elit. Assumenda est accusamus
+              temporibus molestiae in nobis illo officia officiis obcaecati
+              tenetur sequi itaque mollitia ut placeat facere, similique
+              laudantium doloribus velit.
+            </p>
+          </div>
+        </div>
+        <div className="AboutGroup">
+          <img className="hat" src={pirateHat}></img>
+          <div className="AboutTitleAndDescription">
+            <p className="AboutTitle">Zachary</p>
+            <p className="AboutDescription">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Non
+              veritatis dolorum assumenda officia nobis modi eos, neque enim
+              sapiente laborum voluptates asperiores quis reiciendis maxime
+              ducimus mollitia ullam? Placeat, tenetur. Lorem ipsum dolor sit,
+              amet consectetur adipisicing elit. Assumenda est accusamus
+              temporibus molestiae in nobis illo officia officiis obcaecati
+              tenetur sequi itaque mollitia ut placeat facere, similique
+              laudantium doloribus velit.
+            </p>
+          </div>
+        </div>
+        <div className="AboutGroup">
+          <img className="hat" src={colander}></img>
+          <div className="AboutTitleAndDescription">
+            <p className="AboutTitle">Jonathan</p>
+            <p className="AboutDescription">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Non
+              veritatis dolorum assumenda officia nobis modi eos, neque enim
+              sapiente laborum voluptates asperiores quis reiciendis maxime
+              ducimus mollitia ullam? Placeat, tenetur. Lorem ipsum dolor sit,
+              amet consectetur adipisicing elit. Assumenda est accusamus
+              temporibus molestiae in nobis illo officia officiis obcaecati
+              tenetur sequi itaque mollitia ut placeat facere, similique
+              laudantium doloribus velit.
+            </p>
+          </div>
+        </div>
+        <br></br>
+        <br></br>
         <Link to={`/`} className="link">
           Return Home!
         </Link>
